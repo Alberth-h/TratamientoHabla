f12a0a8c1b8826c1473483e1116f51b8784823ec
Alberth-h <jesalbertohyfx@hotmail.com>
2021-03-10 13:54:26 -0700

    Bytes a enteros

--- pythonHabla/readframes.py
+++ pythonHabla/readframes.py
@@ -1,4 +1,5 @@
 import wave
+import numpy as np
 
 #carcar archivo wav en la variable
 gm = wave.open('good-morningMan.wav', 'r')
@@ -8,3 +9,9 @@ frames = gm.readframes(-1)
 
 #mostrar ek resultado de frames
 print(frames[:10])
+
+#Convierte el audio good morning de bytes a enteros
+ondaconvertida = np.frombuffer(frames, dtype='int16')
+
+#Imprime los 10 primeros valores de la onda.
+print(ondaconvertida[:10])
